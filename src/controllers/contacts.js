9386808938877d2dc8cee7d6bb1508415fa4b881
Viktor Atsberher <143943277+vicberger7@users.noreply.github.com>
2024-06-02 16:14:36 +0200

    hw3-crud-hl

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -27,8 +27,7 @@ export const getContactByIdController = async (req, res) => {
 };
 
 export const createContactController = async (req, res) => {
-  const payload = req.body;
-  const newContact = await createContact(payload);
+  const newContact = await createContact(req.body);
 
   res.status(201).json({
     status: 201,
@@ -39,8 +38,7 @@ export const createContactController = async (req, res) => {
 
 export const patchContactController = async (req, res) => {
   const ID = req.params.contactId;
-  const payload = req;
-  const { contact } = await upsertContact(ID, payload);
+  const { contact } = await upsertContact(ID, req.body);
 
   res.status(200).json({
     status: 200,
@@ -51,8 +49,8 @@ export const patchContactController = async (req, res) => {
 
 export const putContactController = async (req, res) => {
   const ID = req.params.contactId;
-  const payload = req;
-  const { contact, isNew } = await upsertContact(ID, payload, {
+  const body = req.body;
+  const { contact, isNew } = await upsertContact(ID, body, {
     upsert: true,
   });
 
